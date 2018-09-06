415a2abb9172e91dbf54e82c92dda2b4afb6d1ac
Neill <neillbogie@googlemail.com>
2018-09-06 16:01:01 +0100

    tidy and move away from var to let and const

--- sketch.js
+++ sketch.js
@@ -1,117 +1,112 @@
 //note: noLoop()
 
-var aliens;
-var gOpts = {};
+let aliens;
+const gOpts = {};
 
-function recreatePalette() {
-  gOpts.palette = Palette.create();
-  gOpts.clearColor = color('black');
+function recreatePalette(){
+	gOpts.palette = Palette.create();
+	gOpts.clearColor = color('black');
 
 }
 
-var Palette = {
-  create: function() {
-    colorMode(HSB, 360, 100, 100, 1);
-    var hue = random(0, 360);
-    var hueWidth = 50;
-    var b = 100;
-    var a = 1;
-    var sat = 60;
-    var cMain = color(hue, sat, b, a);
-    var cLeft = color(hue - hueWidth / 2, sat, b, a);
-    var cRight = color(hue + hueWidth / 2, sat, b, a);
-    var cOpp = color((hue + 180) % 360, sat, b, a);
-    return [cMain, cLeft, cRight, cOpp];
-  }
+const Palette = {
+	create: function(){
+		colorMode(HSB, 360, 100, 100, 1);
+		const hue = random(0,360);
+		const hueWidth = 50;
+		const b = 100;
+		const a = 1;
+		const sat = 60;
+		const cMain = color(hue, sat, b, a);
+		const cLeft = color(hue - hueWidth/2, sat, b, a);
+		const cRight = color(hue + hueWidth/2, sat, b, a);
+		const cOpp = color((hue + 180)%360, sat, b, a);
+		return [cMain, cLeft, cRight, cOpp]; 
+	}
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  recreatePalette();
-  recreateAliens();
-  noLoop();
+	createCanvas(windowWidth, windowHeight);
+	recreatePalette();
+	recreateAliens();
+	noLoop();
 }
 
 
-function recreateAliens() {
-  aliens = [];
-
-  gOpts.padding = 4;
-  gOpts.pixelsInInvader = 5;
-  gOpts.pixelWidth = 5;
-  gOpts.sqDim = gOpts.pixelsInInvader * gOpts.pixelWidth + gOpts.padding * 2;
-
-  gOpts.numRows = Math.floor(windowHeight / gOpts.sqDim);
-  gOpts.numCols = Math.floor(windowWidth / gOpts.sqDim);
-
-  for (var i = 0; i < gOpts.numRows * gOpts.numCols; i++) {
-    aliens.push(createAlien());
-  }
-}
+function recreateAliens(){
+		aliens = [];
+		
+		gOpts.padding = 4;
+		gOpts.pixelsInInvader = 5;
+		gOpts.pixelWidth = 5;
+		gOpts.sqDim = gOpts.pixelsInInvader * gOpts.pixelWidth + gOpts.padding * 2;
+	
+		gOpts.numRows = Math.floor(windowHeight / gOpts.sqDim);
+		gOpts.numCols = Math.floor(windowWidth / gOpts.sqDim);
+		
+		for(let i = 0; i < gOpts.numRows * gOpts.numCols; i++){
+			aliens.push(createAlien());
+		}
+	}
 
 function createAlien() {
-  bits = [];
-  for (var col = 0; col < Math.ceil(gOpts.pixelsInInvader / 2); col++) {
-    bits[col] = [];
-    for (var row = 0; row < gOpts.pixelsInInvader; row++) {
-      bits[col][row] = random([0, 1])
-    }
-  }
-  return {
-    bits: bits,
-    c1: random(gOpts.palette),
-    c0: gOpts.clearColor
-  };
+	const bits = [];
+	for (let col = 0; col < Math.ceil(gOpts.pixelsInInvader / 2); col++){
+		bits[col] = [];
+		for(let row = 0; row < gOpts.pixelsInInvader; row++){
+			bits[col][row] = random([0,1])
+		}
+	}
+	return {bits: bits, c1: random(gOpts.palette), c0: gOpts.clearColor };
 }
 
 function mousePressed() {
-  recreatePalette();
-  recreateAliens();
-  draw();
+	recreatePalette();
+	recreateAliens();
+	draw();
 }
 
-function drawAlien(alien) {
-  noStroke();
-  bits = alien.bits;
-
-  function squareForBit(bit, col, row, c1, c0) {
-    np = gOpts.pixelsInInvader;
-    pw = gOpts.pixelWidth;
-    fill(bit ? c1 : c0);
-    rect(col * pw, row * pw, pw, pw);
-  }
-  var c1 = alien.c1;
-  var c0 = alien.c0;
-
-  np = gOpts.pixelsInInvader;
-  halfnp = Math.ceil(np / 2);
-
-  for (var row = 0; row < np; row++) {
-    for (var col = 0; col < halfnp; col++) {
-      var bit = bits[col][row];
-      squareForBit(bit, col, row, c1, c0);
-    }
-    for (var col = halfnp; col < np; col++) {
-      var bit = bits[np - col - 1][row];
-      squareForBit(bit, col, row, c1, c0);
-    }
-  }
+function drawAlien(alien){
+	noStroke();
+	const bits = alien.bits;
+	
+	function squareForBit(bit, col, row, c1, c0){
+				const np = gOpts.pixelsInInvader;
+				const pw = gOpts.pixelWidth;
+				fill(bit ? c1 : c0);
+				rect(col * pw, row * pw, pw, pw);
+	}
+	const c1 = alien.c1;
+	const c0 = alien.c0;
+		
+	const np = gOpts.pixelsInInvader;
+	const halfnp = Math.ceil(np/2);
+	
+	for(let row = 0; row < np; row++){
+		for (let col = 0; col < halfnp; col++){
+				let bit = bits[col][row];
+				squareForBit(bit, col, row, c1, c0);
+		}
+		for(let col = halfnp; col < np; col++){
+				let bit = bits[np - col - 1][row];
+				squareForBit(bit, col, row, c1, c0);
+		}
+	}
 }
-
 function draw() {
-  background(gOpts.clearColor);
-  push();
-  translate(gOpts.padding, gOpts.padding);
-
-  for (var row = 0; row < gOpts.numRows; row++) {
-    for (var col = 0; col < gOpts.numCols; col++) {
-      if (random() > 0.4) {
-        push();
-        translate(gOpts.sqDim * col, gOpts.sqDim * row);
-        drawAlien(aliens[row * gOpts.numCols + col]);
-        pop();
-      }
-    }
-  }
-  pop();
+	background(gOpts.clearColor);
+	push();	
+	translate(gOpts.padding, gOpts.padding);
+	
+	for(let row = 0; row < gOpts.numRows; row++){
+		for(let col = 0; col < gOpts.numCols; col++){
+			if (random() > 0.4){
+				push();
+				translate(gOpts.sqDim*col,gOpts.sqDim*row);
+				drawAlien(aliens[row*gOpts.numCols + col]);
+				pop();
+			}
+		}
+	}
+	pop();
 }
